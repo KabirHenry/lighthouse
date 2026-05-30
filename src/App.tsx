8c658ee5c5faad7b183f96c46e61baf087a46ef1
Param Siddharth <contact@paramsid.com>
2026-05-30 20:54:37 +0530

    Rename component
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { Route, Routes } from 'react-router';
 import { Container } from 'react-bootstrap';
 
 import './App.css';
+
+import Home from './components/Home';
 import NotFound from './components/NotFound';
-import Welcome from './components/Welcome';
+import Items from './components/Items';
 
 function App() {
 	return (
 		<Container as={'main'} className="py-4 px-3 mx-auto">
 			<Routes>
-				<Route path="/" element={<Welcome />} />
+				<Route path="/" element={<Home />} />
+				<Route path="/items" element={<Items/>} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</Container>
