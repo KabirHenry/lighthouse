5402316b23801422509746cb05f32fc15db6b4eb
Param Siddharth <contact@paramsid.com>
2026-08-23 01:05:30 +0530

    Add onClick and disabled state to button
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,22 @@
 function Button({
 	variant = 'text',
+	disabled = false,
+	onClick,
 	className,
 	children
 }: {
 	variant?: 'icon' | 'text',
+	disabled?: boolean,
+	onClick?: () => void,
 	className?: string,
 	children: React.ReactNode
 }) {
-	return <button className={['btn', `btn-${variant}`, className].filter(Boolean).join(' ')}>{children}</button>;
+	const classes = ['btn', `btn-${variant}`, className].filter(Boolean).join(' ');
+	return <button
+		className={classes}
+		disabled={disabled}
+		onClick={onClick}
+	>{children}</button>;
 }
 
 export default Button;
