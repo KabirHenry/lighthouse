d574ea9c78af40d958ea271715de17653c758a4d
Param Siddharth <contact@paramsid.com>
2026-08-23 15:05:00 +0530

    Add plus button to homes
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- src/components/Homes.tsx
+++ src/components/Homes.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-
+import PlusIcon from '../assets/plus.svg';
 import DustbinIcon from '../assets/dustbin.svg';
 import BackRightIcon from '../assets/back-right.svg';
 import PencilIcon from '../assets/pencil.svg';
@@ -15,8 +15,11 @@ function Homes() {
 		<div className="align-self-end">
 			<IconLink to="/" src={BackRightIcon} alt={t('home.back')} className='bare' />
 		</div>
-		<h1>
-			{t('pages.homes')}
+		<h1 className="main-title">
+			<span className="main-title-text">{t('pages.homes')}</span>
+			<div className="main-title-actions-after">
+				<IconLink to="/homes/new" src={PlusIcon} alt={t('homes.add')} scale='65%' />
+			</div>
 		</h1>
 		<div className="main-buttons home-list d-flex flex-column align-items-center">
 			{
@@ -27,7 +30,7 @@ function Homes() {
 								to={`/homes/${home.id}/delete`}
 								src={DustbinIcon}
 								scale='70%'
-								alt={t('delete')}
+								alt={t('homes.delete')}
 								className='bare d-none d-md-block'
 							/>
 						</div>
@@ -37,14 +40,14 @@ function Homes() {
 								to={`/homes/${home.id}/delete`}
 								src={DustbinIcon}
 								scale='70%'
-								alt={t('delete')}
+								alt={t('homes.delete')}
 								className='bare d-md-none'
 							/>
 							<IconLink
 								to={`/homes/${home.id}`}
 								src={PencilIcon}
 								scale='80%'
-								alt={t('edit')}
+								alt={t('homes.edit')}
 								className='bare'
 							/>
 						</div>
