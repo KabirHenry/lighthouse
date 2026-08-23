ae882d03cc9c6d17a84c3417a247a2dfe1d7b2f9
Param Siddharth <contact@paramsid.com>
2026-08-23 17:16:45 +0530

    Add home-deletion feature
    Signed-off-by: Param Siddharth <contact@paramsid.com>

---
+++ src/components/modals/DeleteHome.tsx
@@ -0,0 +1,44 @@
+import { useTranslation } from 'react-i18next';
+import { useNavigate, useParams } from 'react-router';
+
+import useHomesContext from '../../hooks/useHomesContext';
+import type { HomeID } from '../../services';
+import Button from '../Button';
+import Modal from './Modal';
+
+function DeleteHome() {
+	const { t } = useTranslation();
+	const navigate = useNavigate();
+	const { homes, deleteHome } = useHomesContext();
+	const { id } = useParams<{ id: string }>();
+
+	const home = homes.find((home) => home.id === Number(id));
+
+	const close = () => navigate('/homes');
+
+	const handleConfirm = async () => {
+		if (!id) {
+			return;
+		}
+
+		await deleteHome(Number(id) as HomeID);
+		close();
+	};
+
+	return (
+		<Modal onClose={close}>
+			<div className='d-flex flex-column align-items-center gap-0'>
+				<h2>{t('areYouSure')}</h2>
+				<div className='sheet'>
+					⚠️ {t('homes.deleteConfirm', home)}
+				</div>
+				<div className="app-modal-footer d-flex flex-row justify-content-between w-100">
+					<Button className='confirm' onClick={handleConfirm}>{t('confirm')}</Button>
+					<Button type='button' className='cancel' onClick={close}>{t('cancel')}</Button>
+				</div>
+			</div>
+		</Modal>
+	);
+}
+
+export default DeleteHome;
